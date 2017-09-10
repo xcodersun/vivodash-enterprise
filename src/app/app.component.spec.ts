cd504a9d96306a188a447b491ec5c4bc4cfb7353
asxcoder <xcodersun@gmail.com>
2017-09-09 21:47:51 -0700

    Solve following test error with basic-layout.
    Error: Template parse errors:
    There is no directive with "exportAs" set to "routerLinkActive" ("
         *ngFor="let tab of tabs"
         [routerLink]="tab.link"
         routerLinkActive [ERROR ->]#rla="routerLinkActive"
         [active]="rla.isActive">
         {{tab.label}}
    "): ng:///DynamicTestModule/BasicLayoutComponent.html@4:22
    Can't bind to 'routerLink' since it isn't a known property of 'a'. ("
      <a md-tab-link
         *ngFor="let tab of tabs"
         [ERROR ->][routerLink]="tab.link"
         routerLinkActive #rla="routerLinkActive"
         [active]="rla.isActive">
    "): ng:///DynamicTestModule/BasicLayoutComponent.html@3:5
    Can't bind to 'active' since it isn't a known property of 'a'. ("
         [routerLink]="tab.link"
         routerLinkActive #rla="routerLinkActive"
         [ERROR ->][active]="rla.isActive">
         {{tab.label}}
      </a>
    "): ng:///DynamicTestModule/BasicLayoutComponent.html@5:5

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
-import { MdTabsModule } from '@angular/material';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule,
-        MdTabsModule,
-      ],
       declarations: [
         AppComponent
       ],
--- src/app/dashboard/basic-layout.component.spec.ts
+++ src/app/dashboard/basic-layout.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { BasicLayoutComponent } from './basic-layout.component';
+import { MdTabsModule } from '@angular/material';
 
 describe('BasicLayoutComponent', () => {
   let component: BasicLayoutComponent;
@@ -8,6 +10,10 @@ describe('BasicLayoutComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        MdTabsModule,
+      ],
       declarations: [ BasicLayoutComponent ]
     })
     .compileComponents();
