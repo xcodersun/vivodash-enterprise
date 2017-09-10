e0c25a5760479c9d3d39ac31cfc6e8899cb98bdf
asxcoder <xcodersun@gmail.com>
2017-09-10 11:39:27 -0700

    Add authentication header to http request.

--- src/app/services/student.service.ts
+++ src/app/services/student.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Headers, Http } from '@angular/http';
+import { Router } from '@angular/router';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -8,16 +9,21 @@ import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class StudentService {
-  constructor(private http: Http) { }
-  
+  constructor(
+    private http: Http,
+    private router: Router,
+  ) { }
+
   getStudents() {
+    const headers = new Headers({'Authentication': localStorage.getItem('authtoken')});
     const url = 'http://localhost:8080/api/students';
     return this.http
-               .get(url)
+               .get(url, {headers: headers})
                .map(res => res.json())
                .catch(res => {
                  if (res.status == 401) {
                    localStorage.removeItem('authtoken');
+                   this.router.navigate(['/login']);
                   }
                   return Observable.throw(res);
                 });
