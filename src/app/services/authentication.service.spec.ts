d19528c02c9aaba0685fb025bbf150433cb1d290
asxcoder <xcodersun@gmail.com>
2017-09-24 23:00:48 -0700

    Enable authentication.service unit test.

--- src/app/services/authentication.service.spec.ts
+++ src/app/services/authentication.service.spec.ts
@@ -1,15 +1,20 @@
 import { TestBed, inject } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import { HttpWrapperService } from './http-wrapper.service';
+import { MockHttpWrapperService } from './mock-http-wrapper.service';
 
 describe('AuthenticationService', () => {
-  /*beforeEach(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthenticationService]
+      providers: [
+        { provide: HttpWrapperService, useClass: MockHttpWrapperService },
+        AuthenticationService
+      ]
     });
   });
 
   it('should be created', inject([AuthenticationService], (service: AuthenticationService) => {
     expect(service).toBeTruthy();
-  }));*/
+  }));
 });
