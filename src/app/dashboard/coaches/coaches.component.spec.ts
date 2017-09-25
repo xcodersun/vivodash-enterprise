62b73c8b9ce7c8f79ca4d096a9b9f017239c8d45
asxcoder <xcodersun@gmail.com>
2017-09-24 22:30:39 -0700

    Enabled unit test of coaches.component.

--- src/app/dashboard/coaches/coaches.component.spec.ts
+++ src/app/dashboard/coaches/coaches.component.spec.ts
@@ -1,14 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { DataSource, CdkTableModule } from '@angular/cdk/table';
 
+import { CustomPipesModule } from '../../custom-pipes/custom-pipes.module';
+import { HttpWrapperService } from '../../services/http-wrapper.service';
+import { MockHttpWrapperService } from '../../services/mock-http-wrapper.service';
+import { MaterialModule } from '../../material.module';
+import { CoachService } from '../../services/coach.service';
+import { MOCKCOACHDATA } from '../../test-data/mock-coach-data';
 import { CoachesComponent } from './coaches.component';
 
 describe('CoachesComponent', () => {
-  /*let component: CoachesComponent;
+  let component: CoachesComponent;
   let fixture: ComponentFixture<CoachesComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CoachesComponent ]
+      imports: [
+        CdkTableModule,
+        CustomPipesModule,
+        MaterialModule,
+      ],
+      declarations: [ CoachesComponent ],
+      providers: [
+        { provide: HttpWrapperService, useClass: MockHttpWrapperService },
+        CoachService
+      ],
     })
     .compileComponents();
   }));
@@ -21,5 +37,5 @@ describe('CoachesComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
-  });*/
+  });
 });
